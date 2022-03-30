304903438d588fa25da4dabb139d79b474312f13
MitaliMDeshpande <100403268+MitaliMDeshpande@users.noreply.github.com>
2022-03-31 00:12:53 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,41 @@
+package com.company;
+import java.util.Scanner;
+public class Main {
+    static void CheckPin(String s) throws MyException {
+
+        if(s.length()!=6)
+        {
+            throw new MyException("Invalid Pin Code");
+        }
+        else
+        {
+            System.out.println("Pin Code Accepted!");
+        }
+
+    }
+
+    public static void main(String[] args) {
+        AcceptPin();// write your code here
+    }
+    public static void AcceptPin(){
+        Scanner MyObj=new Scanner(System.in);
+        System.out.println("Please Enter The Pin Code For Delivery:");
+        String s= MyObj.nextLine();
+        try
+        {
+            // calling the method
+            CheckPin(s);
+            System.out.println("Your Order Wil Be Delivered Soon:)...");
+        }
+        catch (MyException ex)
+        {
+            System.out.println("Caught the exception");
+
+            // printing the message from InvalidAgeException object
+            System.out.println("Exception occured: " + ex);
+        }
+
+
+    }
+
+}
---
+++ MyException.java
@@ -0,0 +1,8 @@
+package com.company;
+
+class MyException extends Exception{
+    public MyException(String s){
+        super(s);
+    }
+}
+
